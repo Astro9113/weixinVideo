83649f9d8882eb19ec7980431f7c28a2ab6aa9e4
astro9113 <astro913@163.com>
2018-01-25 14:32:56 +0800

    update

--- router/index.js
+++ router/index.js
@@ -6,7 +6,6 @@ exports.init = function (app) {
 	app.get('/',function(req,res){
 		//var url = req.protocol + '://' + req.host + req.path;
 		var url = req.protocol + '://' + req.host + req.originalUrl; //获取当前url
-        console.log('url', url);
 		signature.sign(url,function(signatureMap){
 			signatureMap.appId = wechat_cfg.appid;
 			res.render('index',signatureMap);
